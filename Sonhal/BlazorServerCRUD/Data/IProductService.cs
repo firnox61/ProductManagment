e76b73fe61f0934eeda080e5b73f9b615104482e
Ismail Enes EROGLU <firnox61@gmail.com>
2022-08-01 11:36:50 +0300

    Search eksik

---
+++ Sonhal/BlazorServerCRUD/Data/IProductService.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorServerCRUD.Data
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task LoadProducts();
+        Task<Product> GetSingleProduct(int UrunId);
+        Task CreateProduct(Product product);
+        Task UpdateProduct(Product product, int UrunId);
+        Task DeleteProduct(int UrunId);
+        
+    }
+}
---
+++ Sonhal/BlazorServerCRUD/Migrations/20220727075024_Init.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorServerCRUD.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    UrunId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrunAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Renk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fiyat = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.UrunId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "UrunId", "Fiyat", "Renk", "UrunAdi" },
+                values: new object[] { 1, 156, "Neon Gri", "Msi Laptop" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "UrunId", "Fiyat", "Renk", "UrunAdi" },
+                values: new object[] { 2, 1886, "Siyah", "Lenova m30" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
