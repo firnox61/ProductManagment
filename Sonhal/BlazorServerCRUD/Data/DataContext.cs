99208c5d2644e0fa196d01ade2cd47e044a03f24
Ismail Enes EROGLU <firnox61@gmail.com>
2022-08-01 11:36:50 +0300

    Search eksik

---
+++ Sonhal/BlazorServerCRUD/Data/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get;set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(pi =>new { pi.UrunId } );
+            //modelBuilder.Entity<Product>().Has
+            modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        UrunId = 1,
+                        UrunAdi = "Msi Laptop",
+                        Renk = "Neon Gri",
+                        Fiyat=156,
+                        
+                    },
+                    new Product
+                    {
+                        UrunId = 2,
+                        UrunAdi = "Lenova m30",
+                        Renk = "Siyah",
+                        Fiyat = 1886,
+                        
+                    }
+                );
+        }
+
+       
+    }
+}
---
+++ Sonhal/BlazorServerCRUD/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerCRUD.Data
+{
+    public class Product
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; } = string.Empty;
+        public string Renk { get; set; } = string.Empty;
+        public int Fiyat { get; set; }
+
+    }
+}
